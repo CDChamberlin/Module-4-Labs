4f2de45396b9474032789a014c09b923be118630
Collin <cdchamberlin.rva@gmail.com>
2023-11-15 21:37:24 -0500

    Added script reference and style reference to main page. Added test data to make sure addCard was working correctly. Added test data for parts 2 and 3 of the lab.

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function addCard(title = "My Card Title", text = "lorem ipsum ble bla") {
   document.querySelector("#card-list").appendChild(template);
 }
 addCard();
+addCard("Test Dynamic", "inserts random text")
+
+// Exercise 2 test data
+const data = [{name: 'bob', age: 23},
+{name: 'alice', age: 39}]
+
+// For exercise 3 use the following:
+const artist = {
+name: "Van Gogh",
+portfolio:[
+{title: "portrait", url:
+"https://collectionapi.metmuseum.org/api/collection/v1/iiif/436532/1671316/main-image"},
+{title: "sky", url:
+"https://mymodernmet.com/wp/wp-content/uploads/2020/11/White-house-night-van-goh-worldwide-2.jpg" },
+]
+}
