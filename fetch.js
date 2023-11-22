409e4429cdc3d50cea4800859e417c262a9db614
Collin <cdchamberlin.rva@gmail.com>
2023-11-22 17:47:16 -0500

    Formatted Lab 6.

--- fetch.js
+++ fetch.js
@@ -1,17 +1,19 @@
 const limit = 10;
 
 axios
-.get("https://jsonplaceholder.typicode.com/posts?_limit="+limit)
-.then((response) => response.data.forEach(element => {
-    addCard(element.title, element.body);
-}))
- 
+  .get("https://jsonplaceholder.typicode.com/posts?_limit=" + limit)
+  .then((response) =>
+    response.data.forEach((element) => {
+      addCard(element.title, element.body);
+    })
+  );
+
 function addCard(title, context) {
-    const template = document
-      .getElementById("card-template")
-      .content.cloneNode(true);
-    // populate the template
-    template.querySelector(".card-title").innerText = title;
-    template.querySelector(".card-text").innerText = context;
-    document.querySelector("#card-list").appendChild(template);
-  }
+  const template = document
+    .getElementById("card-template")
+    .content.cloneNode(true);
+  // populate the template
+  template.querySelector(".card-title").innerText = title;
+  template.querySelector(".card-text").innerText = context;
+  document.querySelector("#card-list").appendChild(template);
+}
