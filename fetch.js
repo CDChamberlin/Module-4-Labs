6ea042c82b4cf47934dd9778f34924a6daf6597b
Collin <cdchamberlin.rva@gmail.com>
2023-11-22 17:42:49 -0500

    Finished Lab 6: Fetching Data

---
+++ fetch.js
@@ -0,0 +1,17 @@
+const limit = 10;
+
+axios
+.get("https://jsonplaceholder.typicode.com/posts?_limit="+limit)
+.then((response) => response.data.forEach(element => {
+    addCard(element.title, element.body);
+}))
+.then((response) => console.log(response));   
+function addCard(title, context) {
+    const template = document
+      .getElementById("card-template")
+      .content.cloneNode(true);
+    // populate the template
+    template.querySelector(".card-title").innerText = title;
+    template.querySelector(".card-text").innerText = context;
+    document.querySelector("#card-list").appendChild(template);
+  }
