7de5e578773d0b9645ec210d61022075d330df45
Collin <cdchamberlin.rva@gmail.com>
2023-11-26 12:35:23 -0500

    Formated documents. Added comments to code.

--- calc.js
+++ calc.js
@@ -1,36 +1,46 @@
 const form = document.querySelector("form");
-//const log = document.querySelector("#log");
 
+// Equals onClick
 function eval(event) {
   event.preventDefault();
-  document.getElementById('result').value = ""
+  document.getElementById("result").value = "";
   let haveInputs = true;
   const data = new FormData(form);
-  const num1 = parseFloat(document.getElementById("num1").value)
-  const num2 = parseFloat(document.getElementById("num2").value)
-  console.log(data.get('operator'))
-  
-  if ((document.getElementById("num1").value.length) < 1) {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  console.log(data.get("operator"));
+  // Validates inputs... Doesn't check that they are valid numbers.
+  if (document.getElementById("num1").value.length < 1) {
     alert("First number missing");
     haveInputs = false;
   }
- if ((document.getElementById("num2").value).length < 1) {
+  if (document.getElementById("num2").value.length < 1) {
     alert("Second number missing");
     haveInputs = false;
   }
-  if (haveInputs){ 
-  switch(data.get('operator')){
-case '+': result = num1+num2; break;
-case '-': result = num1-num2; break;
-case 'x': result = num1*num2; break;
-case '/': result = num1/num2; break;}
+  if (haveInputs) { // Make sure that there are valid inputs before doing calculations
+    switch (data.get("operator")) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "x":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+    }
 
-  document.getElementById('result').value = result}
+    document.getElementById("result").value = result;
+  }
   return false;
 }
-
+// Clear onClick event
 function reset(event) {
   document.getElementById("num1").value = "";
   document.getElementById("num2").value = "";
-  document.getElementById('result').value = ""
+  document.getElementById("result").value = "";
 }
