a11cd81fdef39792f64b6900487ffff204db9032
Collin <cdchamberlin.rva@gmail.com>
2023-11-26 12:20:06 -0500

    Finished Lab 3 Calculator. Data validation only checks for length of at least 1.

--- calc.js
+++ calc.js
@@ -1,18 +1,31 @@
+const form = document.querySelector("form");
+//const log = document.querySelector("#log");
+
 function eval(event) {
   event.preventDefault();
+  document.getElementById('result').value = ""
   let haveInputs = true;
-  console.log("I work");
-  console.log(parseFloat(document.getElementById("num1").value));
-  alert(parseFloat(document.getElementById("num1").value) == NaN);
-  if (parseFloat(document.getElementById("num1").value) == NaN) {
+  const data = new FormData(form);
+  const num1 = parseFloat(document.getElementById("num1").value)
+  const num2 = parseFloat(document.getElementById("num2").value)
+  console.log(data.get('operator'))
+  
+  if ((document.getElementById("num1").value.length) < 1) {
     alert("First number missing");
     haveInputs = false;
   }
-  if (parseFloat(document.getElementById("num2").value) == NaN) {
+ if ((document.getElementById("num2").value).length < 1) {
     alert("Second number missing");
     haveInputs = false;
   }
-  console.log(haveInputs);
+  if (haveInputs){ 
+  switch(data.get('operator')){
+case '+': result = num1+num2; break;
+case '-': result = num1-num2; break;
+case 'x': result = num1*num2; break;
+case '/': result = num1/num2; break;}
+
+  document.getElementById('result').value = result}
   return false;
 }
 
