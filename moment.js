0cd9ef47561d6a6ad17477dce5b6b6d5cf03e786
Collin <cdchamberlin.rva@gmail.com>
2023-11-26 20:41:18 -0500

    Finished part 3.

--- moment.js
+++ moment.js
@@ -1,6 +1,17 @@
 const now = moment();
-const birth = moment("1997-05-15")
+const birth = moment("1997-05-15");
 
-const day = now.diff(birth, 'days') // answers first problem
+const day = now.diff(birth, "days"); // answers first problem
 
-document.getElementById('days').innerHTML = day + 'days';
+document.getElementById("days").innerHTML = day + "days";
+
+function eval(event) {
+  let closest = "";
+  event.preventDefault();
+  const date1 = moment(document.getElementById("num1").value, "MM-DD-YYYY");
+  const date2 = moment(document.getElementById("num2").value, "MM-DD-YYYY");
+  if (Math.abs(now.diff(date1) < Math.abs(now.diff(date2)))) closest = date1;
+  else closest = date2;
+  document.getElementById('result').value = closest
+  return false;
+}
